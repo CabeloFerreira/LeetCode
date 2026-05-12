2bec44008fb204162a0a5ef0898b83eea1c27377
GabrielFerreira <gabrielfs9@hotmail.com>
2026-05-12 12:18:57 -0300

    linked lists are rough

---
+++ mergeTwoSortedLists.js
@@ -0,0 +1,30 @@
+
+// Definition for singly - linked list.
+ function ListNode(val, next) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+        let dummy = new ListNode(0);
+        let atual = dummy;
+
+        while (list1 !== null && list2 !== null) {
+            if (list1.val < list2.val) {
+                atual.next = list1;
+                list1 = list1.next;
+            } else {
+                atual.next = list2;
+                list2 = list2.next;
+            }
+            atual = atual.next;
+        }
+        atual.next = list1 || list2;
+        return dummy.next;
+    }
+
+console.log(mergeTwoLists({ val: 1, next: { val: 2, next: { val: 4, next: null } } }, { val: 1, next: { val: 3, next: { val: 4, next: null } } }));
