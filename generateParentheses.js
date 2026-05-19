706ca747c3199a39233f88d28032b91be8c98a80
GabrielFerreira <gabrielfs9@hotmail.com>
2026-05-18 21:34:43 -0300

    backtracking is rough

---
+++ generateParentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    let resultado = [];
+
+    function backtracking(str, open, close) {
+        if (str.length === 2 * n) {
+            resultado.push(str);
+            return;
+        }
+        if (open < n) {
+            backtracking(str + "(", open + 1, close);
+        }
+        if (close < open) {
+            backtracking(str + ")", open, close + 1);
+        }
+    }
+    backtracking("", 0, 0);
+    return resultado;
+}
+
+console.log(generateParenthesis(3));
